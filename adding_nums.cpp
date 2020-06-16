ad8dc615f02b6dcbac7a5a893092715c9762a2b5
Henry Chou <henry.chou714@gmail.com>
2020-06-16 15:37:47 -0700

    fixed adding_nums file so it runs

---
+++ adding_nums.cpp
@@ -0,0 +1,14 @@
+// this isn't suppose to work, it's just a dummy practice file
+
+#include <iostream>
+
+int main()
+{
+	int x, y, sum;
+	x = 6;
+	y = 7;
+	sum = x + y;
+	std::cout << "this is the sum: " << sum << std::endl;
+
+}
+
